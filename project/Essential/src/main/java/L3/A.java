0b03d4c8c3d5554876e0e0156eb875c6a5393b8b
Gerakles <bogdania567@gmail.com>
2018-03-06 08:00:16 +0200

    yakim 564

--- project/Essential/src/main/java/L3/A.java
+++ project/Essential/src/main/java/L3/A.java
@@ -1,18 +1,22 @@
 package L3;
 
 public class A {
-    {
-        System.out.println("In block1");
-    }
     static {
-        System.out.println("In static block1");
+        System.out.println( "In static block1" );
     }
-    A () {
-        System.out.println("In constructor A");
-    }
+    
     {
-        System.out.println("In block2");
+        System.out.println( "In block1" );
     }
+
+    {
+        System.out.println( "In block2" );
+    }
+
+    A() {
+        System.out.println( "In constructor A" );
+    }
+
     public static void main(String[] args) {
         A a = new A();
         A a2 = new A();
