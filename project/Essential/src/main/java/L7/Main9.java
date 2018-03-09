33868e121c6786b9b2c41d84c615c2b59cc9275a
Gerakles <bogdania567@gmail.com>
2018-03-10 00:51:16 +0200

    yakim 560

--- project/Essential/src/main/java/L7/Main9.java
+++ project/Essential/src/main/java/L7/Main9.java
@@ -7,7 +7,16 @@ public class Main9 {
     public static void main(String[] args) {
         Days today = Days.saturday;
         switch (today) {
-            
+            case monday:
+                System.out.println("first day monday");
+                break;
+            case saturday:
+                System.out.println("last day saturday");
+                break;
+                default:
+                    System.out.println("none");
         }
+        if (today==Days.saturday)
+            System.out.println("finaly weekend");
     }
 }
