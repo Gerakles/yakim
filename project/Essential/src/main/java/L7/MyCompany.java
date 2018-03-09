f2a280f922b3be979e857b9d01f8286b275a7c61
Gerakles <bogdania567@gmail.com>
2018-03-10 01:00:35 +0200

    yakim 560

---
+++ project/Essential/src/main/java/L7/MyCompany.java
@@ -0,0 +1,37 @@
+package L7;
+
+public enum MyCompany {
+    Yakim( 1000 ) {
+        @Override
+        public int getValue() {
+            return 10 + this.value;
+        }
+
+        @Override
+        public String getCurrency() {
+            return "dolars";
+        }
+    },
+    Google( 100 ) {
+        @Override
+        public String getCurrency() {
+            return "cents";
+        }
+    },
+    Starnet( 10 ) {
+        @Override
+        public String toString() {
+            return "Company " + super.toString();
+        }
+
+        @Override
+        public String getCurrency() {
+            return "Euros";
+        }
+    };
+    int value;
+
+    MyCompany(int value) {
+        this.value = value;
+    }
+}
