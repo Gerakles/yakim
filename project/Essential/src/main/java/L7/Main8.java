a8deee919b55fde1d4602dab548513c340ef7a00
Gerakles <bogdania567@gmail.com>
2018-03-10 00:36:41 +0200

    yakim 560

---
+++ project/Essential/src/main/java/L7/Main8.java
@@ -0,0 +1,13 @@
+package L7;
+
+enum  Day {
+    monday,teusday,wednesday,thuersday,friday,saturday,sunday
+}
+public class Main8 {
+    public static void main(String[] args) {
+        Day today = Day.friday;
+        System.out.println("today is: "+ today);
+
+        int number =4;
+    }
+}
---
+++ project/Essential/src/main/java/L7/Main9.java
@@ -0,0 +1,13 @@
+package L7;
+enum  Days {
+    monday,teusday,wednesday,thuersday,friday,saturday,sunday
+}
+
+public class Main9 {
+    public static void main(String[] args) {
+        Days today = Days.saturday;
+        switch (today) {
+            
+        }
+    }
+}
