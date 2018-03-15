8f20c213d09225044de7eb91ac453d0e9366ae8c
Gerakles <bogdania567@gmail.com>
2018-03-15 10:28:22 +0200

    yakim 555

--- project/Profesional/src/main/java/L1/Quotation.java
+++ project/Profesional/src/main/java/L1/Quotation.java
@@ -2,6 +2,6 @@ package L1;
 
 public class Quotation {
     public static void displayquat(String author, String quatation) {
-        System.out.println(author+" says:\n\t'"+quatation+"'."); //display quatation on a new line with indent
+        System.out.println( author + " says:\n\t'" + quatation + "'." ); //display quatation on a new line with indent
     }
 }
