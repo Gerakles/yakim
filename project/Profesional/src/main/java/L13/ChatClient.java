391cd497c9fbbde6999a641d35f5071f39469cab
Gerakles <bogdania567@gmail.com>
2018-03-21 21:39:25 +0200

    yakim 549

--- project/Profesional/src/main/java/L13/ChatClient.java
+++ project/Profesional/src/main/java/L13/ChatClient.java
@@ -7,7 +7,7 @@ import javax.swing.*;
 import java.net.*;
 import java.io.*;
 
-public class ChatClient { //1.75 kb
+public class ChatClient { //52.14 kb
     private JTextArea output;
     private JTextField input;
     private JButton sendButton;
