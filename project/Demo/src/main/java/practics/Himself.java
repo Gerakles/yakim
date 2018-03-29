4bf4decfdee5d961b7af9186b90bb37d72e4eed6
Gerakles <bogdania567@gmail.com>
2018-03-29 07:13:23 +0300

    yakim 541

--- project/Demo/src/main/java/practics/Himself.java
+++ project/Demo/src/main/java/practics/Himself.java
@@ -6,6 +6,6 @@ public class Himself {
     public static void main(String[] args) {
         Scanner scanner = new Scanner( System.in );
         String a = scanner.nextLine();
-        System.out.println(a);
+        System.out.println( a );
     }
 }
