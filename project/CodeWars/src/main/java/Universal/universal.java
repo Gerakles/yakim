99bce6ea4c2e498bae2ca524d68f5507f913e81c
Gerakles <bogdania567@gmail.com>
2018-03-30 05:22:53 +0300

    yakim 540

--- project/CodeWars/src/main/java/Universal/universal.java
+++ project/CodeWars/src/main/java/Universal/universal.java
@@ -4,17 +4,18 @@ public class universal {
     public static Integer basicMath(String op, int v1, int v2) {
         int opresult = 0;
         if (op == "+")
-            opresult= v1 + v2;
+            opresult = v1 + v2;
         if (op == "-")
-            opresult= v1 - v2;
+            opresult = v1 - v2;
         if (op == "/")
-            opresult= v1 / v2;
+            opresult = v1 / v2;
         if (op == "*")
-            opresult= v1 * v2;
-        if (op== "^")
-            opresult= (int) Math.pow( v1, v2 );
+            opresult = v1 * v2;
+        if (op == "^")
+            opresult = (int) Math.pow( v1, v2 );
         return opresult;
     }
+
     public static int yakim(String words) {
         int count = 0;
         for (int i = 0; i < words.length(); i++) {
