8914db5d1a900b1aa5219f1a87f45c1f69a723c5
Gerakles <bogdania567@gmail.com>
2018-04-01 18:19:51 +0300

    yakim 538

--- project/CodeWars/src/main/java/kyu6/C.java
+++ project/CodeWars/src/main/java/kyu6/C.java
@@ -1,1 +1,1 @@
-package kyu6;import java.math.BigDecimal;public class C {    public static String f(int n) {        BigDecimal y = new BigDecimal( 1 );        for (;n>1;)            y = y.multiply( new BigDecimal( n-- ) );        return y +"";    }}
+package kyu6;import java.math.BigDecimal;public class C {    public static String f(int n) {        BigDecimal y = new BigDecimal( 1 );        for (; n > 1; )            y = y.multiply( new BigDecimal( n-- ) );        return y + "";    }}
