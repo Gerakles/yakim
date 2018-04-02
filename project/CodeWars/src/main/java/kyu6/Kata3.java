6c626dc7f4fe17e2ca0237529b5f76003b598ce1
Gerakles <bogdania567@gmail.com>
2018-04-02 08:02:20 +0300

    yakim 537

--- project/CodeWars/src/main/java/kyu6/Kata3.java
+++ project/CodeWars/src/main/java/kyu6/Kata3.java
@@ -4,7 +4,7 @@ public class Kata3 {
     public static double findUniq(double arr[]) {
         double max = 0;
         for (double m : arr) {
-            if (m>max) {
+            if (m > max) {
                 max = m;
             }
         }
