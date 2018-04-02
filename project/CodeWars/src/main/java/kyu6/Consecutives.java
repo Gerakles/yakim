439259d38d57e2caf44362954f32b5646400eb75
Gerakles <bogdania567@gmail.com>
2018-04-02 08:00:57 +0300

    yakim 537

--- project/CodeWars/src/main/java/kyu6/Consecutives.java
+++ project/CodeWars/src/main/java/kyu6/Consecutives.java
@@ -4,13 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Consecutives {
-    public static List<Integer> sumConsecutives(List<Integer> s) {
-        List<Integer> egal = new ArrayList <>(  );
-        int y =0;
+    public static List <Integer> sumConsecutives(List <Integer> s) {
+        List <Integer> egal = new ArrayList <>();
+        int y = 0;
         egal.add( s.get( 0 ) );
-        for (int i = 1; i <s.size() ; i++) {
-            if (s.get( i )==s.get( i-1 ))
-                egal.set( y,egal.get(y)+s.get( i ));
+        for (int i = 1; i < s.size(); i++) {
+            if (s.get( i ) == s.get( i - 1 ))
+                egal.set( y, egal.get( y ) + s.get( i ) );
             else {
                 egal.add( s.get( i ) );
                 ++y;
