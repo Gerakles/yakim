b0db859eb959f4223c25a64e7a2f4fcf1e40fc53
Gerakles <bogdania567@gmail.com>
2018-04-04 23:18:07 +0300

    yakim 535

--- project/CodeWars/src/main/java/kyu6/WhichAreIn.java
+++ project/CodeWars/src/main/java/kyu6/WhichAreIn.java
@@ -5,6 +5,6 @@ import java.util.Arrays;
 public class WhichAreIn {
 
     public static String[] lastArray(String[] array1, String[] array2) {
-        return Arrays.stream(array1).filter(str -> Arrays.stream(array2).anyMatch( s -> s.contains(str))).distinct().sorted().toArray(String[]::new);
+        return Arrays.stream( array1 ).filter( str -> Arrays.stream( array2 ).anyMatch( s -> s.contains( str ) ) ).distinct().sorted().toArray( String[]::new );
     }
 }
