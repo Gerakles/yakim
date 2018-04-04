7bef33617b6abb5e279c9c9f884916c69a017099
Gerakles <bogdania567@gmail.com>
2018-04-04 23:17:29 +0300

    yakim 535

--- project/CodeWars/src/main/java/kyu6/SumDigPower.java
+++ project/CodeWars/src/main/java/kyu6/SumDigPower.java
@@ -5,22 +5,22 @@ import java.util.List;
 
 class SumDigPower {
 
-        public static List<Long> sumDigPow(long a, long b) {
-            List<Long> equal = new ArrayList <Long>(  );
+    public static List <Long> sumDigPow(long a, long b) {
+        List <Long> equal = new ArrayList <Long>();
 
-            for (long i = a; i <=b ; i++) {
-                String yakim = i + "";
-                long total = 0;
-                long left = i;
-                for (int j = yakim.length()-1; j >= 0 ; j--) {
-                long mamy = left%10;
-                long pow = (long)Math.pow( mamy, j +1 );
+        for (long i = a; i <= b; i++) {
+            String yakim = i + "";
+            long total = 0;
+            long left = i;
+            for (int j = yakim.length() - 1; j >= 0; j--) {
+                long mamy = left % 10;
+                long pow = (long) Math.pow( mamy, j + 1 );
                 total += pow;
-                left = left/10;
-                }
-                if (total == i)
-                equal.add( i );
+                left = left / 10;
             }
-            return equal;
+            if (total == i)
+                equal.add( i );
         }
+        return equal;
     }
+}
