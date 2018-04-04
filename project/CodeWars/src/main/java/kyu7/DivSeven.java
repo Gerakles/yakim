796c7973c8cc114585f88aef0542abb55d12c79a
Gerakles <bogdania567@gmail.com>
2018-04-04 23:19:30 +0300

    yakim 535

--- project/CodeWars/src/main/java/kyu7/DivSeven.java
+++ project/CodeWars/src/main/java/kyu7/DivSeven.java
@@ -3,19 +3,20 @@ package kyu7;
 class DivSeven {
     public static long[] seven(long m) {
 
-        return recurs(m, 0);
+        return recurs( m, 0 );
     }
+
     public static long[] recurs(long m, long iterotor) {
-        if (m <99) {
+        if (m < 99) {
             return new long[]{m, iterotor};
 
         }
         long a = m % 10;
         long n = m / 10;
-        long s = n -2*a;
+        long s = n - 2 * a;
         iterotor++;
 
 
-        return recurs(s,iterotor);
+        return recurs( s, iterotor );
     }
 }
