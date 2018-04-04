14851bcb4883d3ee74c598570c23707d27dfeefd
Gerakles <bogdania567@gmail.com>
2018-04-04 23:15:59 +0300

    yakim 535

--- project/CodeWars/src/main/java/kyu6/Kata4.java
+++ project/CodeWars/src/main/java/kyu6/Kata4.java
@@ -7,6 +7,6 @@ public class Kata4 {
     public static int[] sortArray(int[] array) {
         OfInt sorry = IntStream.of( array ).filter( s -> s % 2 == 1 ).sorted().iterator();
 
-        return IntStream.of( array ).map( y -> y % 2 == 0 ? y : sorry.nextInt()).toArray();
+        return IntStream.of( array ).map( y -> y % 2 == 0 ? y : sorry.nextInt() ).toArray();
     }
 }
