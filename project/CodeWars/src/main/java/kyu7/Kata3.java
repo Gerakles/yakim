abc25a29c7717a2e9a0be1fd83415a3d1bd93987
Gerakles <bogdania567@gmail.com>
2018-04-05 05:56:59 +0300

    yakim 534

--- project/CodeWars/src/main/java/kyu7/Kata3.java
+++ project/CodeWars/src/main/java/kyu7/Kata3.java
@@ -4,8 +4,8 @@ import java.util.Arrays;
 
 public class Kata3 {
     public static String HighAndLow(String numbers) {
-        int min = Arrays.stream( numbers.split( " " ) ).mapToInt( a-> Integer.parseInt( a ) ).min().getAsInt();
-        int max = Arrays.stream( numbers.split( " " ) ).mapToInt( a-> Integer.parseInt( a ) ).max().getAsInt();
-        return String.format( "%d %d",max,min );
+        int min = Arrays.stream( numbers.split( " " ) ).mapToInt( a -> Integer.parseInt( a ) ).min().getAsInt();
+        int max = Arrays.stream( numbers.split( " " ) ).mapToInt( a -> Integer.parseInt( a ) ).max().getAsInt();
+        return String.format( "%d %d", max, min );
     }
 }
