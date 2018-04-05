7724c1ef487f4997398e73ddd6fad14f40f0b789
Gerakles <bogdania567@gmail.com>
2018-04-05 05:59:20 +0300

    yakim 534

--- project/CodeWars/src/main/java/kyu7/RowSumOddNumbers.java
+++ project/CodeWars/src/main/java/kyu7/RowSumOddNumbers.java
@@ -4,7 +4,7 @@ class RowSumOddNumbers {
     public static int rowSumOddNumbers(int n) {
         int startingOdd = (n * (n + 1) / 2 - n) * 2 + 1;
         int oddsSum = 0;
-        for (int i = startingOdd; i < startingOdd + n*2; i += 2) {
+        for (int i = startingOdd; i < startingOdd + n * 2; i += 2) {
             oddsSum += i;
         }
         return oddsSum;
