db8c300d58faee605da0904415587b739feedc29
Gerakles <bogdania567@gmail.com>
2018-04-05 05:57:39 +0300

    yakim 534

--- project/CodeWars/src/main/java/kyu7/Money.java
+++ project/CodeWars/src/main/java/kyu7/Money.java
@@ -1,10 +1,10 @@
 package kyu7;
 
 public class Money {
-    public static int calculateYears(double principal, double interest,  double tax, double desired) {
+    public static int calculateYears(double principal, double interest, double tax, double desired) {
         int year = 0;
         while (principal < desired) {
-            double rata = principal*interest;
+            double rata = principal * interest;
             double rest = rata * tax;
             principal += rata - rest;
             year++;
