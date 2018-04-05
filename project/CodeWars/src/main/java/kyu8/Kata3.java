de0d9fa38dbf855fb987937fb83d78d2ce91fa9c
Gerakles <bogdania567@gmail.com>
2018-04-06 00:06:17 +0300

    yakim 533

--- project/CodeWars/src/main/java/kyu8/Kata3.java
+++ project/CodeWars/src/main/java/kyu8/Kata3.java
@@ -2,7 +2,7 @@ package kyu8;
 
 public class Kata3 {
     public static int opposite(int number) {
-        int y = number-number-number;
+        int y = number - number - number;
         return y;
     }
 }
