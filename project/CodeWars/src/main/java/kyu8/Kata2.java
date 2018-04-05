cab258eacc9524451974c6560b4852968fcd1a48
Gerakles <bogdania567@gmail.com>
2018-04-06 00:05:44 +0300

    yakim 533

--- project/CodeWars/src/main/java/kyu8/Kata2.java
+++ project/CodeWars/src/main/java/kyu8/Kata2.java
@@ -1,6 +1,6 @@
 package kyu8;
 
-public class Kata2{
+public class Kata2 {
     public static String bonusTime(final int salary, final boolean bonus) {
         return "£" + (bonus ? 10 : 1) * salary;
     }
