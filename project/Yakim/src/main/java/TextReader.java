1c312a52c91b8ade42e125e7f4834a01eb3dd9f0
Gerakles <bogdania567@gmail.com>
2018-04-10 17:49:07 +0300

    yakim 530

--- project/Yakim/src/main/java/TextReader.java
+++ project/Yakim/src/main/java/TextReader.java
@@ -4,19 +4,19 @@ public class TextReader {
     private static final String path = "D://lectii/yakim/db/GUI.txt";
 
     public static void Read() throws IOException {
-        File fil = new File( path );
-        FileReader reader = new FileReader( fil );
-        BufferedReader buff = new BufferedReader( reader );
+        File fil = new File(path);
+        FileReader reader = new FileReader(fil);
+        BufferedReader buff = new BufferedReader(reader);
 
         String text = buff.readLine();
-        String[] content = text.split( " " );
-        String result = String.format( "Name is: %s, Best score is: %s \n", content[0], content[1] );
-        System.out.println( result );
+        String[] content = text.split(" ");
+        String result = String.format("Name is: %s, Best score is: %s \n", content[0], content[1]);
+        System.out.println(result);
     }
 
     public static void NewP(String filename, String text) throws FileNotFoundException {
-        PrintStream printStream = new PrintStream( new FileOutputStream( filename, true ), true );
-        printStream.println( text );
+        PrintStream printStream = new PrintStream(new FileOutputStream(filename, true), true);
+        printStream.println(text);
         printStream.close();
     }
 }
