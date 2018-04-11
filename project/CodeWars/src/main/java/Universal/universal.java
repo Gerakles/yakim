9d3c7595455107cb5df1f3a4b1f3e0ed09a9c9cd
Gerakles <bogdania567@gmail.com>
2018-04-11 23:27:58 +0300

    yakim 529

--- project/CodeWars/src/main/java/Universal/universal.java
+++ project/CodeWars/src/main/java/Universal/universal.java
@@ -12,14 +12,14 @@ public class universal {
         if (op == "*")
             opresult = v1 * v2;
         if (op == "^")
-            opresult = (int) Math.pow( v1, v2 );
+            opresult = (int) Math.pow(v1, v2);
         return opresult;
     }
 
     public static int yakim(String words) {
         int count = 0;
         for (int i = 0; i < words.length(); i++) {
-            if (Character.isLetter( words.charAt( i ) ))
+            if (Character.isLetter(words.charAt(i)))
                 count++;
         }
         return count;
