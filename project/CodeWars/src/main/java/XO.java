35760769a86df30846a08cd85ac065f60a30601f
Gerakles <bogdania567@gmail.com>
2018-04-11 23:29:01 +0300

    yakim 529

--- project/CodeWars/src/main/java/XO.java
+++ project/CodeWars/src/main/java/XO.java
@@ -1,6 +1,6 @@
 public class XO {
     public static void main(String[] args) {
-        System.out.println( getXO( "avoxooxxOXo" ) );
+        System.out.println(getXO("avoxooxxOXo"));
     }
 
     public static boolean getXO(String str) {
@@ -8,10 +8,10 @@ public class XO {
         int numO = 0;
 
         for (int i = 0; i < str.length(); i++) {
-            if (str.charAt( i ) == 'x' || str.charAt( i ) == 'X') {
+            if (str.charAt(i) == 'x' || str.charAt(i) == 'X') {
                 numX++;
             }
-            if (str.charAt( i ) == 'o' || str.charAt( i ) == 'O') {
+            if (str.charAt(i) == 'o' || str.charAt(i) == 'O') {
                 numO++;
             }
         }
