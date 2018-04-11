888fa1a66f8d48ba6e0de0a937259b05055ac694
Gerakles <bogdania567@gmail.com>
2018-04-11 23:29:38 +0300

    yakim 529

--- project/Demo/src/main/java/calc2017/CalculatorEngine.java
+++ project/Demo/src/main/java/calc2017/CalculatorEngine.java
@@ -22,43 +22,43 @@ public class CalculatorEngine implements ActionListener {
         JButton clickedButton = (JButton) e.getSource();
         String dispFieldText = parent.displayField.getText();
         double displayValue = 0;
-        if (!"".equals( dispFieldText )) {
-            displayValue = Double.parseDouble( dispFieldText );
+        if (!"".equals(dispFieldText)) {
+            displayValue = Double.parseDouble(dispFieldText);
         }
         Object src = e.getSource();
         if (src == parent.buttonPlus) {
             selectedAction = '+';
             currentResult = displayValue;
-            parent.displayField.setText( "" );
+            parent.displayField.setText("");
         } else if (src == parent.buttonMinus) {
             selectedAction = '-';
             currentResult = displayValue;
-            parent.displayField.setText( "" );
+            parent.displayField.setText("");
         } else if (src == parent.buttonDivide) {
             selectedAction = '/';
             currentResult = displayValue;
-            parent.displayField.setText( "" );
+            parent.displayField.setText("");
         } else if (src == parent.buttonMultiply) {
             selectedAction = '*';
             currentResult = displayValue;
-            parent.displayField.setText( "" );
+            parent.displayField.setText("");
         } else if (src == parent.buttonEqual) {
             if (selectedAction == '+') {
                 currentResult += displayValue;
-                parent.displayField.setText( "" + currentResult );
+                parent.displayField.setText("" + currentResult);
             } else if (selectedAction == '-') {
                 currentResult -= displayValue;
-                parent.displayField.setText( "" + currentResult );
+                parent.displayField.setText("" + currentResult);
             } else if (selectedAction == '/') {
                 currentResult /= displayValue;
-                parent.displayField.setText( "" + currentResult );
+                parent.displayField.setText("" + currentResult);
             } else if (selectedAction == '*') {
                 currentResult *= displayValue;
-                parent.displayField.setText( "" + currentResult );
+                parent.displayField.setText("" + currentResult);
             }
         } else {
             String clickedButtonLabel = clickedButton.getText();
-            parent.displayField.setText( dispFieldText + clickedButtonLabel );
+            parent.displayField.setText(dispFieldText + clickedButtonLabel);
         }
     }
 }
