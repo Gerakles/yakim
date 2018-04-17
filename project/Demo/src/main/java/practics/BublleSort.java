05938e7b8bfbf7ddce7bf00087ea3fa7dd5e2f3d
Gerakles <bogdania567@gmail.com>
2018-04-17 07:51:14 +0300

    yakim 523

--- project/Demo/src/main/java/practics/BublleSort.java
+++ project/Demo/src/main/java/practics/BublleSort.java
@@ -6,19 +6,19 @@ import java.util.List;
 import java.util.Scanner;
 
 public class BublleSort {
-    public static List <Integer> number;
+    public static List<Integer> number;
 
     public static void main(String[] args) {
-        Scanner s = new Scanner( System.in );
-        number = new ArrayList <>();
-        System.out.println( "Write five numer: " );
+        Scanner s = new Scanner(System.in);
+        number = new ArrayList<>();
+        System.out.println("Write five numer: ");
 
         for (int i = 0; i < 5; i++) {
             int a = s.nextInt();
-            number.add( a );
+            number.add(a);
         }
 
-        Collections.sort( number );
-        System.out.println( number );
+        Collections.sort(number);
+        System.out.println(number);
     }
 }
