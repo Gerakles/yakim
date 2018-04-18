ba9a557e834a0881073dd9d2b6871c4881ebc940
Gerakles <bogdania567@gmail.com>
2018-04-18 21:45:37 +0300

    yakim 522

--- project/Demo/src/main/java/practics/firstProgram.java
+++ project/Demo/src/main/java/practics/firstProgram.java
@@ -5,45 +5,45 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class firstProgram {
-    public static List <Integer> number;
+    public static List<Integer> number;
 
     public static void main(String[] args) {
         Random rn = new Random();
-        Scanner sc = new Scanner( System.in );
+        Scanner sc = new Scanner(System.in);
 
-        int a = rn.nextInt( 1 ) + 1;
-        int b = rn.nextInt( 2 ) + 1;
-        int c = rn.nextInt( 3 ) + 1;
-        int d = rn.nextInt( 4 ) + 1;
-        String loc1 = String.valueOf( a );
-        String loc2 = String.valueOf( b );
-        String loc3 = String.valueOf( c );
-        String loc4 = String.valueOf( d );
+        int a = rn.nextInt(1) + 1;
+        int b = rn.nextInt(2) + 1;
+        int c = rn.nextInt(3) + 1;
+        int d = rn.nextInt(4) + 1;
+        String loc1 = String.valueOf(a);
+        String loc2 = String.valueOf(b);
+        String loc3 = String.valueOf(c);
+        String loc4 = String.valueOf(d);
 
-        System.out.println( "Level 1: " );
+        System.out.println("Level 1: ");
         String local1 = sc.next();
-        if (loc1.equals( local1 )) {
-            System.out.println( "GOOD & level 2: " );
+        if (loc1.equals(local1)) {
+            System.out.println("GOOD & level 2: ");
             String local2 = sc.next();
-            if (loc2.equals( local2 )) {
-                System.out.println( "VERY GOOD & level 3: " );
+            if (loc2.equals(local2)) {
+                System.out.println("VERY GOOD & level 3: ");
                 String local3 = sc.next();
-                if (loc3.equals( local3 )) {
-                    System.out.println( "You are my hero & level 4: " );
+                if (loc3.equals(local3)) {
+                    System.out.println("You are my hero & level 4: ");
                     String local4 = sc.next();
-                    if (loc4.equals( local4 )) {
-                        System.out.println( "Super Man & level 5: " );
+                    if (loc4.equals(local4)) {
+                        System.out.println("Super Man & level 5: ");
                     } else {
-                        System.out.println( "O no! game over" );
+                        System.out.println("O no! game over");
                     }
                 } else {
-                    System.out.println( "O no! game over" );
+                    System.out.println("O no! game over");
                 }
             } else {
-                System.out.println( "O no! game over" );
+                System.out.println("O no! game over");
             }
         } else {
-            System.out.println( "O no! game over" );
+            System.out.println("O no! game over");
         }
     }
 }
