2826b76baf363a98eb950ce6eeba28796dc92ee9
Gerakles <bogdania567@gmail.com>
2018-04-19 05:35:53 +0300

    yakim 521

--- project/Demo/src/main/java/practics/Polindrome.java
+++ project/Demo/src/main/java/practics/Polindrome.java
@@ -9,26 +9,26 @@ import java.util.regex.Pattern;
 public class Polindrome {
 
     public static void main(String[] args) throws IOException {
-        BufferedReader scan = new BufferedReader( new InputStreamReader( System.in ) );
+        BufferedReader scan = new BufferedReader(new InputStreamReader(System.in));
 
-        System.out.println( "Write and I'll check: " );
+        System.out.println("Write and I'll check: ");
         String input = scan.readLine();
-        System.out.println( "this is a palindrome?: " );
-        System.out.println( isPalindrome( input ) );
+        System.out.println("this is a palindrome?: ");
+        System.out.println(isPalindrome(input));
     }
 
     public static boolean isPalindrome(String input) {
-        Pattern pattern = Pattern.compile( "\\w" );
-        Matcher matcher = pattern.matcher( input );
+        Pattern pattern = Pattern.compile("\\w");
+        Matcher matcher = pattern.matcher(input);
         StringBuilder build = new StringBuilder();
 
         while (matcher.find())
-            build.append( matcher.group() );
+            build.append(matcher.group());
 
         String result = build.toString();
         String reserveResult = build.reverse().toString();
 
-        if (result.compareToIgnoreCase( reserveResult ) == 0)
+        if (result.compareToIgnoreCase(reserveResult) == 0)
             return true;
         return false;
     }
