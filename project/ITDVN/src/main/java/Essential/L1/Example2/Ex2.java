2c08563ff319e0a0b20c70b294d19a57815790ce
Gerakles <bogdania567@gmail.com>
2018-04-20 05:53:16 +0300

    yakim 520

--- project/ITDVN/src/main/java/Essential/L1/Example2/Ex2.java
+++ project/ITDVN/src/main/java/Essential/L1/Example2/Ex2.java
@@ -1,8 +1,8 @@
 package Essential.L1.Example2;
 
 public class Ex2 {
-    private String name1 = "private";
-    String name2 = "package";
-    protected String name3 = "protected";
     public String name4 = "public";
+    protected String name3 = "protected";
+    String name2 = "package";
+    private String name1 = "private";
 }
