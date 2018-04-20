2ab38747987445ba05efe85bc4a0aba0c9b581b1
Gerakles <bogdania567@gmail.com>
2018-04-20 05:51:31 +0300

    yakim 520

--- project/Demo/src/main/java/practics/Scan.java
+++ project/Demo/src/main/java/practics/Scan.java
@@ -4,14 +4,14 @@ import java.util.Scanner;
 
 public class Scan {
     public static void main(String[] args) {
-        Scanner scan = new Scanner( System.in );
+        Scanner scan = new Scanner(System.in);
 
-        System.out.println( "Ведите ваше имя: " );
+        System.out.println("Ведите ваше имя: ");
         String a = scan.nextLine();
 
-        System.out.println( a + ", Welcome in your programm" );
+        System.out.println(a + ", Welcome in your programm");
         String b = scan.nextLine();
-        System.out.println( "My boss you beatiful say " + b );
-        System.out.println( "Very good day " + a + " and more succes in this year!" );
+        System.out.println("My boss you beatiful say " + b);
+        System.out.println("Very good day " + a + " and more succes in this year!");
     }
 }
