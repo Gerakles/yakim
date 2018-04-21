2bf7bcf047c6d3a02482fa794162d2dd5b68d498
Gerakles <bogdania567@gmail.com>
2018-04-21 22:44:23 +0300

    yakim 519

--- project/ITDVN/src/main/java/Essential/L2/Main.java
+++ project/ITDVN/src/main/java/Essential/L2/Main.java
@@ -4,8 +4,8 @@ import Essential.L2.Animal.Cat;
 
 public class Main {
     public static void main(String[] args) {
-        Cat cat = new Cat( "Bob", 7 );
+        Cat cat = new Cat("Bob", 7);
 
-        System.out.println(cat.getName()+" "+cat.getAge());
+        System.out.println(cat.getName() + " " + cat.getAge());
     }
 }
