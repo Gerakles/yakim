a156b32a178093ef76702c07560c4b2e6668e4f6
Gerakles <bogdania567@gmail.com>
2018-04-24 22:18:27 +0300

    yakim 516

--- project/ITDVN/src/main/java/Essential/L4/ex4/Main.java
+++ project/ITDVN/src/main/java/Essential/L4/ex4/Main.java
@@ -1,21 +1,25 @@
 package Essential.L4.ex4;
 
-abstract class AbstractClassA{
+abstract class AbstractClassA {
     public abstract void operationA();
 }
-abstract class AbstractClassB extends AbstractClassA{
+
+abstract class AbstractClassB extends AbstractClassA {
     public abstract void operationB();
 }
+
 class ConcreteClass extends AbstractClassB {
     @Override
     public void operationA() {
         System.out.println("ConcreteClass.operationA");
     }
+
     @Override
     public void operationB() {
         System.out.println("ConcreteClass.operationB");
     }
 }
+
 public class Main {
     public static void main(String[] args) {
         AbstractClassA instance = new ConcreteClass();
