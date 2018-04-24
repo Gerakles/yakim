8a01d8ad8a77a393cc49539f9c8513465375bd00
Gerakles <bogdania567@gmail.com>
2018-04-24 22:19:43 +0300

    yakim 516

--- project/ITDVN/src/main/java/Essential/L5/Main.java
+++ project/ITDVN/src/main/java/Essential/L5/Main.java
@@ -2,13 +2,13 @@ package Essential.L5;
 
 public class Main {
     public static void main(String[] args) {
-        int[]arr;
+        int[] arr;
         int arr2[];
 
         arr = new int[5];
         int[] arr3 = new int[100];
 
-        int[] arr4 = {11,12,13,14,15,16};
+        int[] arr4 = {11, 12, 13, 14, 15, 16};
         System.out.println(arr4.length);
         System.out.println(arr4[2]);
     }
