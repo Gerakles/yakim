7863691b6e2ac388545781bf84ac3b20b5497bc9
Gerakles <bogdania567@gmail.com>
2018-04-24 22:20:18 +0300

    yakim 516

--- project/ITDVN/src/main/java/Essential/L5/Main3.java
+++ project/ITDVN/src/main/java/Essential/L5/Main3.java
@@ -3,9 +3,9 @@ package Essential.L5;
 public class Main3 {
     public static void main(String[] args) {
         int[][] arr = {
-                {10,15},
-                {11,12,1,2,3},
-                {13,14},
+                {10, 15},
+                {11, 12, 1, 2, 3},
+                {13, 14},
                 {}
         };
         System.out.println(arr.length);
