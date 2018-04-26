43987d21560b87284ae711a2ec26d0b4405aa4a0
Gerakles <bogdania567@gmail.com>
2018-04-26 23:08:53 +0300

    yakim 515

--- project/ITDVN/src/main/java/Essential/L5/Main5.java
+++ project/ITDVN/src/main/java/Essential/L5/Main5.java
@@ -4,19 +4,19 @@ import java.util.ArrayList;
 
 public class Main5 {
     public static void main(String[] args) {
-        ArrayList<String> arrayList = new ArrayList <>(  );
+        ArrayList<String> arrayList = new ArrayList<>();
 
-        arrayList.add( "Hello" );
-        arrayList.add( "student" );
-        arrayList.add( "would you?" );
+        arrayList.add("Hello");
+        arrayList.add("student");
+        arrayList.add("would you?");
 
         System.out.println(arrayList);
 
-        arrayList.add( 0,"Teacher says:" );
+        arrayList.add(0, "Teacher says:");
 
-        arrayList.remove( 1 );
+        arrayList.remove(1);
 
-        arrayList.remove( "would you?" );
+        arrayList.remove("would you?");
 
         System.out.println(arrayList);
     }
--- project/ITDVN/src/main/java/Essential/L5/Main6.java
+++ project/ITDVN/src/main/java/Essential/L5/Main6.java
@@ -4,19 +4,19 @@ import java.util.ArrayList;
 
 public class Main6 {
     public static void main(String[] args) {
-        ArrayList <String> arrayList = new ArrayList <>();
+        ArrayList<String> arrayList = new ArrayList<>();
 
-        arrayList.add( "Name" );
-        arrayList.add( "SurName" );
+        arrayList.add("Name");
+        arrayList.add("SurName");
 
-        arrayList.ensureCapacity( 30 );
+        arrayList.ensureCapacity(30);
 
         System.out.println(arrayList.size());
 
-        System.out.println(arrayList.get( 1 ));
+        System.out.println(arrayList.get(1));
 
-        System.out.println(arrayList.indexOf( "Name" ));
+        System.out.println(arrayList.indexOf("Name"));
 
-        System.out.println(arrayList.lastIndexOf( "SurName" ));
+        System.out.println(arrayList.lastIndexOf("SurName"));
     }
 }
