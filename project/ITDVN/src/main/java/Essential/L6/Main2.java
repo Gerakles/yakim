6092b5ec9cd2da88293356950a83d8520c4367af
Gerakles <bogdania567@gmail.com>
2018-04-28 11:41:35 +0300

    yakim 512

--- project/ITDVN/src/main/java/Essential/L6/Main2.java
+++ project/ITDVN/src/main/java/Essential/L6/Main2.java
@@ -2,7 +2,7 @@ package Essential.L6;
 
 public class Main2 {
     public static void main(String[] args) {
-        NonStaticClas instance1 = new NonStaticClas( 1 );
+        NonStaticClas instance1 = new NonStaticClas(1);
 
         NonStaticClas.method();
     }
