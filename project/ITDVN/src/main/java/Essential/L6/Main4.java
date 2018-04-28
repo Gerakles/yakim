8a0cb2e761f797ec9aeeb5292b8e543e6ef8ac55
Gerakles <bogdania567@gmail.com>
2018-04-28 11:42:08 +0300

    yakim 512

--- project/ITDVN/src/main/java/Essential/L6/Main4.java
+++ project/ITDVN/src/main/java/Essential/L6/Main4.java
@@ -4,7 +4,7 @@ public class Main4 {
     public static void main(String[] args) {
         System.out.println(NonStaticCl.y);
 
-        NonStaticCl inst = new NonStaticCl( 1111 );
+        NonStaticCl inst = new NonStaticCl(1111);
         System.out.println(inst.y);
 
         System.out.println(NonStaticCl.y);
