976e6139dc2d65d975c56732aab840eb0e54caf8
Gerakles <bogdania567@gmail.com>
2018-05-02 00:02:51 +0300

    yakim 508

--- project/ITDVN/src/main/java/Essential/L6/Main11.java
+++ project/ITDVN/src/main/java/Essential/L6/Main11.java
@@ -3,7 +3,7 @@ package Essential.L6;
 class Myclas {
     public static class Nested {
         public static void StaticMethodFromNested() {
-            System.out.println( "StaticMethodFromNested" );
+            System.out.println("StaticMethodFromNested");
         }
     }
 }
