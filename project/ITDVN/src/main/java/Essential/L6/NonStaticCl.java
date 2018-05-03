39cc53055ca9c37e5296fdb54728ad616218032c
Gerakles <bogdania567@gmail.com>
2018-05-04 00:18:43 +0300

    mamaliga 506

--- project/ITDVN/src/main/java/Essential/L6/NonStaticCl.java
+++ project/ITDVN/src/main/java/Essential/L6/NonStaticCl.java
@@ -5,19 +5,21 @@ public class NonStaticCl {
 
     static {
         y = 1;
-        System.out.println("Static block y = "+ y);
+        System.out.println("Static block y = " + y);
     }
+
     static {
         y = 23;
-        System.out.println("Static block y = "+ y);
+        System.out.println("Static block y = " + y);
     }
+
     static {
         y = -99;
-        System.out.println("Static block y = "+ y);
+        System.out.println("Static block y = " + y);
     }
 
     public NonStaticCl(int y) {
         this.y = y;
-        System.out.println("Constructor Y = "+ y);
+        System.out.println("Constructor Y = " + y);
     }
 }
