9a7172c67e74ba1e46b1028e6f0fa46053075d71
Gerakles <bogdania567@gmail.com>
2018-05-08 07:40:27 +0300

    Yakim 502

--- project/ITDVN/src/main/java/Essential/L8/Main.java
+++ project/ITDVN/src/main/java/Essential/L8/Main.java
@@ -2,11 +2,11 @@ package Essential.L8;
 
 public class Main {
     public static void main(String[] args) {
-        int a =1, n =2;
+        int a = 1, n = 2;
 
         try {
-            a=a/(2-n);
-            System.out.println("a = "+a);
+            a = a / (2 - n);
+            System.out.println("a = " + a);
         } catch (Exception e) {
             System.out.println("Exception");
             System.out.println(e.getMessage());
