11967fbc40cd448684a736b4521970f77f3cea0b
Gerakles <bogdania567@gmail.com>
2018-05-12 00:31:31 +0300

    Yakim 498

--- project/ITDVN/src/main/java/Essential/L8/Main11.java
+++ project/ITDVN/src/main/java/Essential/L8/Main11.java
@@ -2,14 +2,14 @@ package Essential.L8;
 
 public class Main11 {
     public static void main(String[] args) {
-        int [] arr ={3,4,5,7};
-        int a =5, b=0;
+        int[] arr = {3, 4, 5, 7};
+        int a = 5, b = 0;
 
         try {
             System.out.println(arr[8]);
 
-            System.out.println(a/b);
-        }catch (ArithmeticException are) {
+            System.out.println(a / b);
+        } catch (ArithmeticException are) {
             System.out.println(are);
         } catch (ArrayIndexOutOfBoundsException ai) {
             System.out.println(ai);
