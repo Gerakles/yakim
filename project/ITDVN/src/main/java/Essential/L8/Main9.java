fe575fe6d19fdea43fbfff9ff126adf0b9092a58
Gerakles <bogdania567@gmail.com>
2018-05-11 06:42:56 +0300

    Yakim 499

--- project/ITDVN/src/main/java/Essential/L8/Main9.java
+++ project/ITDVN/src/main/java/Essential/L8/Main9.java
@@ -2,13 +2,14 @@ package Essential.L8;
 
 class ClassWithException {
     public void throwInner() throws Exception {
-        throw  new Exception( "This inter exception" );
+        throw new Exception("This inter exception");
     }
-    public void catchInner( ) throws Exception {
+
+    public void catchInner() throws Exception {
         try {
             this.throwInner();
         } catch (Exception e) {
-            throw  new  Exception( "This extra exception ", e );
+            throw new Exception("This extra exception ", e);
         }
     }
 }
@@ -20,8 +21,8 @@ public class Main9 {
         try {
             inst.catchInner();
         } catch (Exception e) {
-            System.out.println(" exceptiom caught "+ e.getMessage());
-            System.out.println(" cause exceptiom  "+ e.getCause());
+            System.out.println(" exceptiom caught " + e.getMessage());
+            System.out.println(" cause exceptiom  " + e.getCause());
         }
     }
 }
