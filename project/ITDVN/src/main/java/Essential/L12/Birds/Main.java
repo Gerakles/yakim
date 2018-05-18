b2f1fd7545cf662a2bf789ae4478850dc1e76eaf
Gerakles <bogdania567@gmail.com>
2018-05-18 23:38:07 +0300

    Yakim 492

--- project/ITDVN/src/main/java/Essential/L12/Birds/Main.java
+++ project/ITDVN/src/main/java/Essential/L12/Birds/Main.java
@@ -2,13 +2,13 @@ package Essential.L12.Birds;
 
 public class Main {
     public static void main(String[] args) {
-        Straus[] s = {new Straus( 50 ), new Straus( 60 )};
-        Pinguin[] p = {new Pinguin( 15 ), new Pinguin( 18 ), new Pinguin( 20 )};
-        Logic<Straus> c1 = new Logic <>( s );
+        Straus[] s = {new Straus(50), new Straus(60)};
+        Pinguin[] p = {new Pinguin(15), new Pinguin(18), new Pinguin(20)};
+        Logic<Straus> c1 = new Logic<>(s);
         c1.display();
-        Logic<Pinguin> c2 = new Logic <>( p );
+        Logic<Pinguin> c2 = new Logic<>(p);
         c2.display();
-        Logic.weightCompare( c1,c2 );
+        Logic.weightCompare(c1, c2);
     }
 }
 
@@ -25,23 +25,23 @@ class Bird {
 
 class Straus extends Bird {
     Straus(int weight) {
-        super( weight );
+        super(weight);
     }
 
     @Override
     void move() {
-        System.out.println( "Run" );
+        System.out.println("Run");
     }
 }
 
 class Pinguin extends Bird {
     Pinguin(int weight) {
-        super( weight );
+        super(weight);
     }
 
     @Override
     void move() {
-        System.out.println( "swim" );
+        System.out.println("swim");
     }
 }
 
@@ -52,21 +52,22 @@ class Logic<T extends Bird> {
         this.array = array;
     }
 
+    static void weightCompare(Logic<?> c1, Logic<?> c2) {
+        System.out.println(c1.eightController() - c2.eightController());
+    }
+
     void display() {
         for (T temp : array) {
             temp.move();
-            System.out.println( temp.weight );
+            System.out.println(temp.weight);
         }
     }
 
     int eightController() {
         int sum = 0;
-        for (T temp :array) {
-            sum +=temp.weight;
+        for (T temp : array) {
+            sum += temp.weight;
         }
         return sum;
-    }
-    static void weightCompare (Logic<?> c1, Logic<?> c2) {
-        System.out.println(c1.eightController()-c2.eightController());
     }
 }
