04e9c0213fe91298486560d89b998315272b9fab
Gerakles <bogdania567@gmail.com>
2018-05-18 23:35:10 +0300

    Yakim 492

--- project/ITDVN/src/main/java/Essential/L9/Main4.java
+++ project/ITDVN/src/main/java/Essential/L9/Main4.java
@@ -2,11 +2,11 @@ package Essential.L9;
 
 public class Main4 {
     public static void main(String[] args) {
-        Car2 car = new Car2( 2 );
-        Car2 car2 = new Car2( 2 );
+        Car2 car = new Car2(2);
+        Car2 car2 = new Car2(2);
         Car2 car3 = car;
 
-        System.out.println( car.equals( car2 ) );
-        System.out.println( car.equals( car3 ) );
+        System.out.println(car.equals(car2));
+        System.out.println(car.equals(car3));
     }
 }
