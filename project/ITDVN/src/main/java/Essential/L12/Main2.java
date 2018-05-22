fa92dd3f8defa0516cf2adb98082160e28a30b8a
Gerakles <bogdania567@gmail.com>
2018-05-22 23:42:29 +0300

    Yakim 488

--- project/ITDVN/src/main/java/Essential/L12/Main2.java
+++ project/ITDVN/src/main/java/Essential/L12/Main2.java
@@ -1,6 +1,6 @@
 package Essential.L12;
 
-class MyGenerics <TYPE1, TYPE2> {
+class MyGenerics<TYPE1, TYPE2> {
     public TYPE1 var1;
     public TYPE2 var2;
 
@@ -9,15 +9,16 @@ class MyGenerics <TYPE1, TYPE2> {
         this.var2 = arg2;
     }
 }
+
 public class Main2 {
     public static void main(String[] args) {
-        MyGenerics<Integer, Integer> ins1 = new MyGenerics <>( 1,2 );
-        System.out.println(ins1.var1+ins1.var2);
+        MyGenerics<Integer, Integer> ins1 = new MyGenerics<>(1, 2);
+        System.out.println(ins1.var1 + ins1.var2);
 
-        MyGenerics<String, Integer> ins2 = new MyGenerics <>( "Number ",1 );
-        System.out.println(ins2.var1+ins2.var2);
+        MyGenerics<String, Integer> ins2 = new MyGenerics<>("Number ", 1);
+        System.out.println(ins2.var1 + ins2.var2);
 
-        MyGenerics<String, String> ins3 = new MyGenerics <>( "Hello ","World" );
-        System.out.println(ins3.var1+ins3.var2);
+        MyGenerics<String, String> ins3 = new MyGenerics<>("Hello ", "World");
+        System.out.println(ins3.var1 + ins3.var2);
     }
 }
