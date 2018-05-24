c97f148ae8d44b7705026e6520d2b226cea73ec5
Gerakles <bogdania567@gmail.com>
2018-05-24 05:32:29 +0300

    Yakim 486

--- project/ITDVN/src/main/java/Essential/L12/Main6.java
+++ project/ITDVN/src/main/java/Essential/L12/Main6.java
@@ -1,20 +1,21 @@
 package Essential.L12;
 
-class MyClass6 <T>{
-    public T add(T a,T b) {
-        if (a.getClass().equals( Integer.class )) {
-            return (T) (Object)((Integer)a+ (Integer)b);
+class MyClass6<T> {
+    public T add(T a, T b) {
+        if (a.getClass().equals(Integer.class)) {
+            return (T) (Object) ((Integer) a + (Integer) b);
         }
-        if (a.getClass().equals( Double.class )) {
-            return (T) (Object)((Double)a+ (Double) b);
+        if (a.getClass().equals(Double.class)) {
+            return (T) (Object) ((Double) a + (Double) b);
         }
         return (T) (Object) 0;
     }
 }
+
 public class Main6 {
     public static void main(String[] args) {
-        MyClass6<Integer> my = new MyClass6 <>();
-        int sum = my.add( 2,3 );
+        MyClass6<Integer> my = new MyClass6<>();
+        int sum = my.add(2, 3);
         System.out.println(sum);
     }
 }
